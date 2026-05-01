8e651bc67462fd7c005dae89a1bdd5dcf21d2965
moniross <monirfouzi20@gmail.com>
2026-05-01 13:15:09 +0100

    update response status and remove duplicated code

--- srcs/http/HandlePost.cpp
+++ srcs/http/HandlePost.cpp
@@ -56,9 +56,9 @@ void Response::handleLogin(const Request& request) {
 void Response::multiPart(Request& request, const MultipartField& part, std::string uploadDir) {
 	if (part.filename.empty()) {
 		request.setFormData(part.name, part.data);
-		setStatus(HTTP_201_CREATED);
+		setStatus(HTTP_200_OK);
 		setHeader("Content-Type", "text/plain");
-		setBody("");
+		setBody(part.name + ": " + part.data);
 		return;
 	}
 
@@ -93,7 +93,6 @@ void Response::handlePost(Request& request) {
 		return;
 	}
 
-	// CGI check FIRST — before upload guard
 	if (request.hasCgi()) {
 		const std::string fullPath = request.getResolveFullPath();
 		Cgi				  cgi(request, *request.getConf(), locConf, fullPath);
@@ -122,24 +121,29 @@ void Response::handlePost(Request& request) {
 
 	static unsigned long uploadCounter = 0;
 	std::string			 type		   = request.getHeader("content-type");
+	bool upload = false;
+	std::string bodyStr = "Form data received successfully.\n";
 	if (type.find("multipart/form-data") != std::string::npos) {
-		std::string bodyStr = "Form data received successfully.\n";
 		for (size_t i = 0; i < request.getMultipartFields().size(); i++) {
 			multiPart(request, request.getMultipartFields()[i], uploadDir);
-			if (getStatusCode() != HTTP_201_CREATED)
+			if (getStatusCode() == HTTP_201_CREATED)
+				upload = true;
+			if (getStatusCode() != HTTP_201_CREATED && getStatusCode() != HTTP_200_OK)
 				return;
 			bodyStr += getBody();
 		}
+		if (upload)
+			setStatus(HTTP_201_CREATED);
 		setBody(bodyStr + "\n");
 	} else if (type == "application/x-www-form-urlencoded") {
 		setStatus(HTTP_200_OK);
 		setHeader("Content-Type", "text/plain");
-		setBody("Form data received successfully.\n");
+		Request::FormData data = request.getFormData();
+		for (Request::FormData::const_iterator it = data.begin(); it != data.end(); ++it) {
+			bodyStr += it->first + ": " + it->second + "\n";
+		}
+		setBody(bodyStr);
 	} else {
-		std::string uploadDir = locConf->upload_path;
-		if (!uploadDir.empty() && uploadDir[uploadDir.size() - 1] != '/')
-			uploadDir += '/';
-
 		std::ostringstream oss;
 		oss << uploadDir << "upload_" << std::time(NULL) << "_" << uploadCounter++;
 
@@ -161,12 +165,4 @@ void Response::handlePost(Request& request) {
 		setHeader("Content-Type", "text/plain");
 		setBody("File uploaded: " + filePath + "\n");
 	}
-	// for debuging
-	// std::map<std::string, std::string> myMap = request.getFormKeyValue();
-	// for (std::map<std::string, std::string>::iterator it = myMap.begin();
-	// 	it != myMap.end(); ++it)
-	// {
-	// 	std::cout << "||||||||||||||||||||||| key:" << it->first << " value:" << it->second <<
-	// std::endl;
-	// }
 }
